3539f45cedf16bef7a70fb196244663ac26bcbd7
Biswanath Modak <biswanathit05@gmail.com>
2025-08-30 02:10:39 +0530

    Implement database connection and user model schema

---
+++ backend/src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+export const connectDB = async (app) => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    app.on("error", (error) => {
+      console.log("ERROR", error);
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`Database connected successfully`);
+      console.log(`Server is running on http://localhost:${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import { connectDB } from "./db/index.js";
 import app from "./app.js";
-const PORT = process.env.PORT || 8001;
 
-const startServer = async () => {
-  try {
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to start server:", error.message);
-    process.exit(1);
-  }
-};
-
-startServer();
+connectDB(app);
