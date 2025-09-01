29aa102e2f3ff486d5b1f010356f6a8c6761d263
Biswanath Modak <biswanathit05@gmail.com>
2025-09-01 21:37:18 +0530

    Change in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use(
     origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["*"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
